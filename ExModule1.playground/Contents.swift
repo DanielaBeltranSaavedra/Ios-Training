02e587e0681fd5439c9b5aeae55dfe7f89e67f1b
DanielaBeltranSaavedra <daniela.beltran@globant.com>
2021-10-25 14:37:42 -0500

    Add function to get the count of numbers inside the array that are lower or greater than the second param

--- ExModule1.playground/Contents.swift
+++ ExModule1.playground/Contents.swift
@@ -1,17 +1,5 @@
 import UIKit
 
-// ex1
-let distance: UInt = 41
-
-switch distance{
-case 0: print("Here")
-case 1...5: print("Immediate vicinity")
-case 5...15: print("Near")
-case 15...40: print("Kind of far")
-case 40...: print("Far")
-default: print("The number is less than zero");<#code#>
-}
-
 //ex2
 
 func countLessGreater(myArr: [Int], number: Int) {
