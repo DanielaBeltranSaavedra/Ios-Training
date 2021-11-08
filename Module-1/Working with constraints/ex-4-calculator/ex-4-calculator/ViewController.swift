f0740a416255079b89dc958f718482250eb3ea54
DanielaBeltranSaavedra <daniela.beltran@globant.com>
2021-11-08 08:19:58 -0500

    Revert "Revert "Revert "Calculator view"""
    This reverts commit 3acf4e5d69344bcffefca5564311ac436bb11ee3.

--- Module-1/Working with constraints/ex-4-calculator/ex-4-calculator/ViewController.swift
+++ Module-1/Working with constraints/ex-4-calculator/ex-4-calculator/ViewController.swift
@@ -9,75 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var result: UILabel!
-    @IBOutlet weak var acBtn: UIButton!
-    @IBOutlet weak var plusMinusBtn: UIButton!
-    @IBOutlet weak var percentageBtn: UIButton!
-    @IBOutlet weak var divBtn: UIButton!
-    @IBOutlet weak var sevenBtn: UIButton!
-    @IBOutlet weak var eightBtn: UIButton!
-    @IBOutlet weak var nineBtn: UIButton!
-    var operationResult: Float = 0
-    var selectNumber: Float = 0
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("hi")
-        result.text = String (0)
-
         // Do any additional setup after loading the view.
     }
-    @IBAction func selectSeven(_ sender: UIButton) {
-        selectNumber = 7
-        if(operationResult == 0){
-            result.text = String (selectNumber)
-            operationResult = selectNumber
-            
-        }else{
-            print(operationResult)
-            result.text = String(Int(operationResult)) + String(Int(selectNumber))
-           // operationResult = Float(result.text)
-        }
-    }
-    
-    @IBAction func selectEight(_ sender: UIButton) {
-        operationResult = 8
-        result.text = String (8)
-    }
-    
-    @IBAction func selectNine(_ sender: UIButton) {
-        operationResult = 9
-        result.text = String (9)
-    }
-    
-    @IBAction func selectFour(_ sender: UIButton) {
-        operationResult = 4
-        result.text = String (4)
-    }
-    @IBAction func selectFive(_ sender: UIButton) {
-        operationResult = 5
-        result.text = String (5)
-    }
-    
-    @IBAction func selectSix(_ sender: UIButton) {
-        operationResult = 6
-        result.text = String (6)
-    }
-    @IBAction func selectOne(_ sender: UIButton) {
-        operationResult = 1
-        result.text = String (1)
-    }
-    @IBAction func selectTwo(_ sender: UIButton) {
-        operationResult = 2
-        result.text = String (2)
-    }
-    @IBAction func selectThree(_ sender: UIButton) {
-        operationResult = 3
-        result.text = String (3)
-    }
-    @IBAction func selectZero(_ sender: UIButton) {
-        operationResult = 0
-        result.text = String (0)
-    }
+
+
 }
 
